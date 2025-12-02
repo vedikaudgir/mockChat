365e3a260ea01aae0ad0ccfe27da4f858587d7e8
Vedika Udgir <vedikaudgir@gmail.com>
2025-12-02 19:31:53 +0530

    added just the frontend

--- server/main.py
+++ server/main.py
@@ -13,8 +13,22 @@ from database import (
 )
 from schemas import MessageSchema, BulkMessageSchema, HeaderSchema
 from utils.renderers.whatsapp import renderWhatsApp
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="MockChat Generator")
+
+origins = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
